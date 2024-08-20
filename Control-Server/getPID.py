1d8e88051d8970f15875cf3910de39498ce448f7
KashishKap <144962856+KashishKap@users.noreply.github.com>
2024-08-19 22:15:48 -0700

    PID getiing

---
+++ Control-Server/getPID.py
@@ -0,0 +1,47 @@
+import socket
+import time
+import subprocess
+
+import socketio
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+sock.bind(('0.0.0.0', 6666))
+
+sio = socketio.Client()
+
+
+@sio.event
+def connect():
+    print("PID connected!")
+
+
+@sio.event
+def disconnect():
+    print("PIDdisconnected!")
+
+
+def runStuff():
+    sio.connect("http://localhost:5001", transports=["websocket"])
+    time.sleep(0.2)
+
+    try:
+        while True:
+            sock.settimeout(0.1)  # Set the timeout to  1 second
+            try:
+                data, server = sock.recvfrom(6666)
+                print(f"Received: {data.decode()}")
+                sio.emit("joystick", str(data.decode()))
+
+            except socket.timeout:
+                print(".", end="")
+
+    except KeyboardInterrupt:
+        print("Closing socket")
+        sock.close()
+
+    finally:
+        print("Closing socket")
+        sock.close()
+        # plt.ioff()  # Turn off interactive mode
+        # plt.show()  # Show the plot
